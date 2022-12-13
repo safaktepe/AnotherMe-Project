3488e8a71fccf78637c20a2ea6d7892ba5d88bf0
safaktepe <safaktepemert@gmail.com>
2022-12-13 13:13:06 +0300

    Imageview with a  edit button attached to itself created for profile edit page.

--- AnotherMe - 90 Days Challange/Views/ViewControllers/FeedBackViewController.swift
+++ AnotherMe - 90 Days Challange/Views/ViewControllers/FeedBackViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class FeedBackViewController: UIViewController {
+    let myImageViewCornerRadius: CGFloat = 40.0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            
+        let myImageView = UIImageView()
+        
+        let myImageViewCornerRadius: CGFloat = 75.0
+        
+        // Just using a color for the example...
+        myImageView.backgroundColor = .blue
+        myImageView.layer.cornerRadius = myImageViewCornerRadius
+        myImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myImageView)
+        
+        let myEditButton = UIButton()
+        // Again using a color...
+        myEditButton.backgroundColor = .red
+        myEditButton.translatesAutoresizingMaskIntoConstraints = false
+        myEditButton.layer.cornerRadius = 10.0
+        view.addSubview(myEditButton)
+        
+        NSLayoutConstraint.activate([
+                    myImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                    myImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                    myImageView.widthAnchor.constraint(equalToConstant: 150.0),
+                    myImageView.heightAnchor.constraint(equalToConstant: 150.0),
+                    
+                    myEditButton.centerYAnchor.constraint(equalTo: myImageView.bottomAnchor,
+                                                          constant: -myImageViewCornerRadius / 4.0),
+                    
+                    myEditButton.centerXAnchor.constraint(equalTo: myImageView.trailingAnchor,
+                                                         constant: -myImageViewCornerRadius / 4.0),
+                    
+                    myEditButton.widthAnchor.constraint(equalToConstant: 20.0),
+                    myEditButton.heightAnchor.constraint(equalToConstant: 20.0)
+                ])
+         
+                self.view = view
+        
     }
 }
