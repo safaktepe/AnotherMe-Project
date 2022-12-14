bc29c4ea2a9a9ac87e04301f7ccdf56db8be7d08
safaktepe <safaktepemert@gmail.com>
2022-12-14 12:30:05 +0300

    Calender page redesigned. Collectionview implemented.

---
+++ AnotherMe - 90 Days Challange/Views/CustomView/CalendarCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCollectionViewCell.swift
+//  AnotherMe - 90 Days Challange
+//
+//  Created by Mert Şafaktepe on 14.12.2022.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var daysLabel: UILabel!
+    
+}
--- AnotherMe - 90 Days Challange/Views/ViewControllers/CalenderViewController.swift
+++ AnotherMe - 90 Days Challange/Views/ViewControllers/CalenderViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 
 class CalenderViewController: UIViewController {
+    var totalSquaeres = [String]()
+
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 1...75 {
+            totalSquaeres.append("\(i)")
+        }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
     @IBAction func faqButtonClicked(_ sender: Any) {
         performSegue(withIdentifier: "help", sender: nil)
 
+    }
+}
+
+
+extension CalenderViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquaeres.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCollectionViewCell
+        cell.daysLabel.text = totalSquaeres[indexPath.row]
+        return cell
+    }
+}
+
+extension CalenderViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let totalSize                      = collectionView.frame.size
+        let screenWidth                    = totalSize.width
+        let screenHeight                   = totalSize.height
+        
+        return CGSize(width: (screenWidth - (11 * 2.0)) / 10, height: (screenHeight - (12 * 1.0)) / 11)
     }
 }
