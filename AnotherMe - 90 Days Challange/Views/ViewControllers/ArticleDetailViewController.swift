7d0f46f0b60c80fae1fbe6f1f35e0a3676389546
safaktepe <safaktepemert@gmail.com>
2023-02-01 18:24:34 +0300

    Gradient from transparent to black for imageview feature implemented. Bug on iPad screen's fixed. (UI)

--- AnotherMe - 90 Days Challange/Views/ViewControllers/ArticleDetailViewController.swift
+++ AnotherMe - 90 Days Challange/Views/ViewControllers/ArticleDetailViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ArticleDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var gradientContainerView: UIView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel : UILabel!
+    @IBOutlet weak var titleLabel       : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setGradientLayer()
+        
+        
     }
-   
+    
+    fileprivate func setGradientLayer() {
+        // Transparent to black gradient for imageView's bottom.
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.frame
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.0, 0.4]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientContainerView.layer.addSublayer(gradientLayer)
+    }
 }
